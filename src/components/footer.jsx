1eda47990fe77624c92489cd9347077713bf0994
Mayank Suman <mayank@simpplr.com>
2021-04-23 23:55:06 +0530

    change footer link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,15 +3,10 @@ import React from "react"
 const Footer = () => {
   return (
     <footer className="my-12 text-center">
-      © {new Date().getFullYear()}, Built with
+      © {new Date().getFullYear()}, Built by
       {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a> and{" "}
-      <a
-        href="https://github.com/renyuanz/leonids"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Leonids theme
+      <a href="https://www.mayanksuman.dev" target="blank">
+        Mayank Suman
       </a>
       .
     </footer>
